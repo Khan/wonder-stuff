e6631a5636bf84c2ef34cc749d0eb67aff255079
Jeff Yates <jeff@khanacademy.org>
2021-10-07 16:45:59 -0500

    No need to reference @babel/plugin-proposal-class-properties since preset-env includes it already

--- build-settings/babel.config.js
+++ build-settings/babel.config.js
@@ -1,9 +1,6 @@
 /* eslint-disable import/no-commonjs */
 module.exports = {
     presets: ["@babel/preset-flow", "@babel/preset-env"],
-    plugins: [
-        "@babel/plugin-proposal-class-properties",
-    ],
     env: {
         test: {
             presets: [
