c4711b2d456a5921b5b4041ed633c4504f23c47b
Jeff Yates <jeff@khanacademy.org>
2021-10-08 18:24:47 -0500

    Fix test

--- packages/wonder-stuff-core/src/__tests__/index.test.js
+++ packages/wonder-stuff-core/src/__tests__/index.test.js
@@ -1,14 +1,29 @@
 // @flow
 describe("index.js", () => {
-    it("should log hello world on import", () => {
-        // Arrange
-        const spy = jest.spyOn(console, "log");
-
-        // Act
-        // eslint-disable-next-line import/no-unassigned-import
-        require("../index.js");
-
-        // Assert
-        expect(spy).toHaveBeenCalledWith("Hello World!");
+    const NODE_ENV = process.env.NODE_ENV;
+    afterEach(() => {
+        if (NODE_ENV) {
+            process.env.NODE_ENV = NODE_ENV;
+        } else {
+            delete process.env.NODE_ENV;
+        }
     });
+
+    it.each(["production", "development", "test"])(
+        "should log hello world on import",
+        (nodeEnv) => {
+            // Arrange
+            process.env.NODE_ENV = nodeEnv;
+            const spy = jest.spyOn(console, "log");
+
+            // Act
+            jest.isolateModules(() => {
+                // eslint-disable-next-line import/no-unassigned-import
+                require("../index.js");
+            });
+
+            // Assert
+            expect(spy).toHaveBeenCalledWith(`Hello ${nodeEnv} World!`);
+        },
+    );
 });
