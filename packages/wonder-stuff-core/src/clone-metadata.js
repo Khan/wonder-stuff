7e7577dae36488025b01dad57f053caa39c48a06
Jeff Yates <jeff@khanacademy.org>
2021-11-11 14:47:16 -0600

    [fei4142.1] Export the clone function per earlier review suggestion

--- packages/wonder-stuff-core/src/clone-metadata.js
+++ packages/wonder-stuff-core/src/clone-metadata.js
@@ -1,22 +1,6 @@
 // @flow
+import {clone} from "./clone.js";
 import type {Metadata} from "./types.js";
-
-const cloneValue = <T>(value: T): T => {
-    if (value != null && typeof value === "object") {
-        if (Array.isArray(value)) {
-            return value.map(cloneValue);
-        } else {
-            return Object.keys(value).reduce((acc, key) => {
-                acc[key] = cloneValue(value[key]);
-                return acc;
-                // We need to cast to any here so that the value is a valid
-                // match for T in the return type after the accumulator is done.
-                // $FlowIgnore[unclear-type]
-            }, ({}: any));
-        }
-    }
-    return value;
-};
 
 /**
  * Clone the given metadata.
@@ -33,10 +17,10 @@ export const cloneMetadata = <T: Metadata>(
     }
 
     // Clone the data.
-    const clone = cloneValue(metadata);
+    const clonedValue = clone(metadata);
 
     // We know that we have cloned the incoming data and so our clone is of
     // the correct type.
     // $FlowIgnore[incompatible-return]
-    return Object.freeze(clone);
+    return Object.freeze(clonedValue);
 };
