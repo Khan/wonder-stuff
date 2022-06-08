42f5ac67cb94a38a8f6caaad59a6d860873f2f44
Kevin Barabash <kevinb@khanacademy.org>
2022-06-08 14:08:53 -0400

    Fix pre-publish check to work with eslint-config-khan

--- utils/pre-publish-check-ci.js
+++ utils/pre-publish-check-ci.js
@@ -2,6 +2,7 @@
 /**
  * Pre-publish checks to verify that our publish will go smoothly.
  */
+const fs = require("fs");
 const path = require("path");
 const fg = require("fast-glob");
 
@@ -18,6 +19,19 @@ fg(path.join(__dirname, "..", "packages", "**", "package.json")).then(
         // eslint-disable-next-line promise/always-return
         for (const pkgPath of pkgPaths) {
             const pkgJson = require(path.relative(__dirname, pkgPath));
+
+            // Skip over packages don't have a build step
+            if (pkgJson.nobuild) {
+                const mainPath = path.join(path.dirname(pkgPath), pkgJson.main);
+                if (!fs.existsSync(mainPath)) {
+                    const {name, main} = pkgJson;
+                    console.error(
+                        `ERROR: ${name}'s "main" field is set to ${main}, but no file exists at that path.`,
+                    );
+                    process.exit(1);
+                }
+                continue;
+            }
 
             if (!checkPrivate(pkgJson)) {
                 checkPublishConfig(pkgJson);
