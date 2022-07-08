46081c641fa2ad1078dd4aff673eabd9253df795
Jeff Yates <jeff@khanacademy.org>
2022-07-08 15:26:09 -0500

    [fei4636.2] Fix docs

--- packages/wonder-stuff-core/src/keys.js
+++ packages/wonder-stuff-core/src/keys.js
@@ -2,7 +2,7 @@
 /**
  * Return an array of the enumerable keys of an object.
  *
- * @param {$ReadOnly<{[mixed]: mixed}>} obj The object for which the values are
+ * @param {$ReadOnly<{[string]: mixed}>} obj The object for which the values are
  * to be returned.
  * @returns {Array<$Keys<O>>} An array of the enumerable keys of an object.
  */
