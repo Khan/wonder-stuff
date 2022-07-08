6aad47592a4353d4a31b13977c5765eb81576123
Jeff Yates <jeff@khanacademy.org>
2022-07-08 15:23:02 -0500

    [fei4636.2] Add keys() method

---
+++ packages/wonder-stuff-core/src/keys.js
@@ -0,0 +1,11 @@
+// @flow
+/**
+ * Return an array of the enumerable keys of an object.
+ *
+ * @param {$ReadOnly<{[mixed]: mixed}>} obj The object for which the values are
+ * to be returned.
+ * @returns {Array<$Keys<O>>} An array of the enumerable keys of an object.
+ */
+export function keys<O: {[string]: mixed}>(obj: O): Array<$Keys<O>> {
+    return Object.keys(obj);
+}
