4a2bca54ef1b05be1f410cd1caecbb783516077c
Jeff Yates <jeff@khanacademy.org>
2022-07-11 13:11:52 -0500

    [fei4636.3] Add entries() method (#316)
    ## Summary:
    This adds a strongly typed `entries()` method to use in place of `Object.entries()`
    
    Issue: FEI-4636
    
    ## Test plan:
    `yarn test`
    `yarn flow`
    
    Author: somewhatabstract
    
    Reviewers: jeresig, somewhatabstract, kevinbarabash, benchristel, maddy531
    
    Required Reviewers:
    
    Approved By: jeresig, kevinbarabash, benchristel
    
    Checks: ⌛ gerald, ⌛ Prime node_modules cache for primary configuration (ubuntu-latest, 16.x), ⌛ Analyze (javascript), ⏭  dependabot
    
    Pull Request URL: https://github.com/Khan/wonder-stuff/pull/316

---
+++ .changeset/and-this-too.md
@@ -0,0 +1,5 @@
+---
+"@khanacademy/wonder-stuff-core": minor
+---
+
+Add `entries` method as strongly-typed alternative to `Object.entries`
---
+++ packages/wonder-stuff-core/src/entries.js
@@ -0,0 +1,14 @@
+// @flow
+/**
+ * Return an array of key/value tuples for an object.
+ *
+ * @param {$ReadOnly<{[K]: V}>} obj The object for which the values are
+ * to be returned.
+ * @returns {Array<[K, V]>} An array of key/value tuples for the object.
+ */
+export function entries<K, V>(obj: $ReadOnly<{[K]: V}>): Array<[K, V]> {
+    // This cast is deliberate as Object.entries is typed to return
+    // Array<[string, mixed]>, but we want to return Array<[K, V]>.
+    // $FlowIgnore[unclear-type]
+    return (Object.entries(obj): any);
+}
