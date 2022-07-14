5107540e6d611410a2020dd72151b44b95a9c1f0
Jeff Yates <jeff@khanacademy.org>
2022-07-14 12:15:50 -0500

    [fixkeystypes] Tweak types a little differently, per review commentary

--- packages/wonder-stuff-core/src/keys.js
+++ packages/wonder-stuff-core/src/keys.js
@@ -2,10 +2,10 @@
 /**
  * Return an array of the enumerable keys of an object.
  *
- * @param {$ReadOnly<{[string]: V}>} obj The object for which the values are
+ * @param {$ReadOnly<{[string]: mixed}>} obj The object for which the values are
  * to be returned.
  * @returns {Array<$Keys<O>>} An array of the enumerable keys of an object.
  */
-export function keys<V, O: {[string]: V}>(obj: O): Array<$Keys<O>> {
+export function keys<O: {[string]: mixed}>(obj: $ReadOnly<O>): Array<$Keys<O>> {
     return Object.keys(obj);
 }
