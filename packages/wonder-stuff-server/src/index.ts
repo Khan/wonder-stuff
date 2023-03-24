8c19dfb5766c8ae403fbe9fddf19910bbc5e8ae3
Jeff Yates <jeff@khanacademy.org>
2023-03-24 12:39:31 -0500

    [fei4960.audit.1] Make sure to export the things that the render-gateway needs

--- packages/wonder-stuff-server/src/index.ts
+++ packages/wonder-stuff-server/src/index.ts
@@ -2,10 +2,9 @@ export type {LogLevel, Logger, ServerOptions, RequestWithLog} from "./types";
 export {Runtime} from "./types";
 
 export {Errors} from "./errors";
-export {createLogger} from "./create-logger";
+export {decryptBufferWithKms} from "./decrypt-buffer-with-kms";
+export {getAgentForURL} from "./get-agent-for-url";
+export {getAppEngineInfo} from "./get-app-engine-info";
 export {getLogger} from "./get-logger";
-export {getRequestLogger} from "./get-request-logger";
 export {getRuntimeMode} from "./get-runtime-mode";
 export {startServer} from "./start-server";
-export {getAgentForURL} from "./get-agent-for-url";
-export {decryptBufferWithKms} from "./decrypt-buffer-with-kms";
