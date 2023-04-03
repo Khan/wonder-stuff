fccb30783c4ca6668f61c6f3a4fc956dbc77fc66
Jeff Yates <jeff@khanacademy.org>
2023-04-03 15:35:07 -0500

    [rg.audit.3] Fix keys typing

--- packages/wonder-stuff-core/src/__tests__/keys.typestest.ts
+++ packages/wonder-stuff-core/src/__tests__/keys.typestest.ts
@@ -22,7 +22,6 @@ import {keys} from "../keys";
     // It would be nice if this worked, but TypeScript's library definition
     // defines the return type of Object.keys() to be Array<string>.
     const keys2bad = keys(obj2);
-    // @ts-expect-error: string is not assignable to "a" | "b" | "c"
     const _: "a" | "b" | "c" = keys2bad[0];
 
     // @ts-expect-error: This errors because we try to get a key of only one type.
--- packages/wonder-stuff-core/src/keys.ts
+++ packages/wonder-stuff-core/src/keys.ts
@@ -1,12 +1,11 @@
 /**
  * Return an array of the enumerable keys of an object.
  *
- * @param {$ReadOnly<interface {[string]: mixed}>} obj The object for which the values are
+ * @param {object} obj The object for which the values are
  * to be returned.
  * @returns {Array<$Keys<O>>} An array of the enumerable keys of an object.
  */
 // NOTE(kevinb): This type was copied from TypeScript's library definitions.
-// eslint-disable-next-line @typescript-eslint/ban-types
-export function keys(obj: {}): string[] {
-    return Object.keys(obj);
+export function keys<O extends object>(obj: O): Array<keyof O> {
+    return Object.keys(obj) as Array<keyof O>;
 }
