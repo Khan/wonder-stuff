346805923527796f63a6364f769ee768da2ed773
Jeffrey Lau <>
2023-04-12 14:52:44 -0700

    updated based on PR comments

--- packages/wonder-stuff-ci/src/types.ts
+++ packages/wonder-stuff-ci/src/types.ts
@@ -5,3 +5,29 @@ type MobileReleaseBranchInfo = {
     prefix: string;
     version: string;
 };
+
+/**
+ * The options for the Winston GCP transport.
+ */
+type GCPTransportOptions = {
+    /**
+     * The GCP project ID to send logs to.
+     */
+    projectId: string;
+    /**
+     * The name of the log to send logs to in GCP.
+     */
+    logName: string;
+    /**
+     * If true, logs will be written to stdout instead of GCP
+     */
+    redirectToStdout: boolean;
+    /**
+     * The log level to send to GCP.
+     */
+    level: string;
+    /**
+     * K/V metadata that will be add to the logs
+     */
+    labels: {[key: string]: string};
+};
