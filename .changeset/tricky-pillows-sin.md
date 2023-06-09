a3ba9db531f43d796d9b96ee62ebe68420ec0306
Jeffrey Lau <34088613+jlauzy@users.noreply.github.com>
2023-06-09 14:46:46 -0700

    Cleaned up the code and fix the logger for new required type by GCP API (#768)
    ## Summary:
    - The logger no longer works because GCP's API made a breaking change where the resource type is required.
    - Cleaned up the code to have more flexibility for these changes by exposing more configuration to the client.
    
    ![image](https://github.com/Khan/wonder-stuff/assets/34088613/284361a2-66cf-4530-a0cb-cc98ab5df3a1)
    
    I'm pretty sure they are going to make a change to their SDK so that this is no longer nullable but required
    ![image](https://github.com/Khan/wonder-stuff/assets/34088613/40a43925-4d81-4416-ad6b-7186ce449163)
    
    
    Issue: XXX-XXXX
    
    ## Test plan:
    Works Again!
    ![image](https://github.com/Khan/wonder-stuff/assets/34088613/e1223935-c04d-46a6-b366-4f30328368fe)
    
    Author: jlauzy
    
    Reviewers: jlauzy, jeresig
    
    Required Reviewers:
    
    Approved By: jeresig
    
    Checks: ✅ codecov/project, ✅ Test (macos-latest, 16.x), ✅ CodeQL, ✅ Lint, typecheck, and coverage check (ubuntu-latest, 16.x), ✅ gerald, ✅ Prime node_modules cache for primary configuration (ubuntu-latest, 16.x), ✅ Analyze (javascript), ⏭  dependabot
    
    Pull Request URL: https://github.com/Khan/wonder-stuff/pull/768

---
+++ .changeset/tricky-pillows-sin.md
@@ -0,0 +1,5 @@
+---
+"@khanacademy/wonder-stuff-ci": minor
+---
+
+Update the library to fix GCP api changes
--- packages/wonder-stuff-ci/src/types.ts
+++ packages/wonder-stuff-ci/src/types.ts
@@ -27,9 +27,50 @@ export type GCPTransportOptions = {
      */
     level: string;
     /**
-     * K/V metadata that will be add to the logs
+     * K/V labels that will be add to the logs
      */
     labels: {[key: string]: string};
+    /**
+     * K/V for setting the resource values for GCP
+     */
+    resource: {[key: string]: string};
 };
 
+/**
+ * The gcp log levels that are supported.
+ */
 export type GCPLogLevels = "debug" | "info" | "warn" | "error";
+
+/**
+ * The options for the mobile release logger.
+ */
+export type MobileReleaseLoggerOptions = {
+    /**
+     * If true, logs will be written to stdout.
+     */
+    redirectToStdout?: boolean;
+    /**
+     * K/V labels that will be add to the logs
+     */
+    labels?: {[key: string]: string};
+    /**
+     * The minimum log level to send to GCP.
+     */
+    logLevel?: GCPLogLevels;
+    /**
+     * The name of the log to send logs to in GCP.
+     */
+    logName?: string;
+    /**
+     * The GCP project ID to send logs to.
+     */
+    projectId?: string;
+    /**
+     * K/V metadata that will be add to each the log
+     */
+    defaultMetadata?: {[key: string]: string};
+    /**
+     * K/V for setting the resource values for GCP, where resource.type is required by GCP API.
+     */
+    resource?: {[key: string]: string};
+};
