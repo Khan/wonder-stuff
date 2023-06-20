b317d6fbfa1f6724ac874444369c7db356fca2d5
Kevin Barabash <kevinb@khanacademy.org>
2023-06-20 11:16:22 -0400

    Fix exports of isTruthy and isNonNullable

--- packages/wonder-stuff-core/src/index.ts
+++ packages/wonder-stuff-core/src/index.ts
@@ -8,7 +8,7 @@ export {keys} from "./keys";
 export {KindError} from "./kind-error";
 export {safeStringify} from "./safe-stringify";
 export {truncateMiddle} from "./truncate-middle";
-export * from "./type-predicates";
+export {isTruthy, isNonNullable} from "./type-predicates";
 export {values} from "./values";
 export {secret} from "./secrets/secret";
 export {UnreachableCaseError} from "./unreachable-case-error";
