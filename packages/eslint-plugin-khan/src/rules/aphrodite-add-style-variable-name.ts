61ab9e075e37d3fa519bd8a9549c578b2e765f22
Bea Esguerra <beaesguerra@khanacademy.org>
2024-12-12 15:49:28 -0700

    Favour early returns instead of nested if statements

--- packages/eslint-plugin-khan/src/rules/aphrodite-add-style-variable-name.ts
+++ packages/eslint-plugin-khan/src/rules/aphrodite-add-style-variable-name.ts
@@ -30,44 +30,56 @@ export default createRule<Options, MessageIds>({
     create(context) {
         return {
             VariableDeclarator(node: TSESTree.VariableDeclarator) {
-                // Check if addStyle is being called
+                // Return early if it isn't calling a function
+                if (node.init?.type !== AST_NODE_TYPES.CallExpression) {
+                    return;
+                }
+                // Return early if addStyle is not being called
                 if (
-                    node.init &&
-                    node.init.type === AST_NODE_TYPES.CallExpression &&
-                    node.init.callee.type === AST_NODE_TYPES.Identifier &&
-                    node.init.callee.name === "addStyle"
+                    node.init.callee.type !== AST_NODE_TYPES.Identifier ||
+                    node.init.callee.name !== "addStyle"
                 ) {
-                    // Get variable name for the addStyle return value
-                    const variableName =
-                        node.id.type === AST_NODE_TYPES.Identifier
-                            ? node.id.name
-                            : null;
+                    return;
+                }
 
-                    // Get the tag name that was passed into addStyle
-                    const firstArg = node.init.arguments[0];
-                    if (
-                        firstArg &&
-                        firstArg.type === "Literal" &&
-                        typeof firstArg.value === "string"
-                    ) {
-                        const tagName = firstArg.value;
-                        const expectedName = `Styled${tagName
-                            .charAt(0)
-                            .toUpperCase()}${tagName.slice(1)}`;
+                // Get variable name for the addStyle return value
+                const variableName =
+                    node.id.type === AST_NODE_TYPES.Identifier
+                        ? node.id.name
+                        : null;
 
-                        // Check if the variable name matches the expected pattern
-                        if (variableName !== expectedName) {
-                            context.report({
-                                node: node.id,
-                                messageId: "errorString",
-                                data: {
-                                    variableName,
-                                    tagName,
-                                    expectedName,
-                                },
-                            });
-                        }
-                    }
+                // Return early if there is no variable name
+                if (variableName === null) {
+                    return;
+                }
+
+                // Get the tag name that was passed into addStyle
+                const firstArg = node.init.arguments[0];
+
+                // Return early if the argument is not a string literal
+                if (
+                    firstArg?.type !== AST_NODE_TYPES.Literal ||
+                    typeof firstArg.value !== "string"
+                ) {
+                    return;
+                }
+
+                const tagName = firstArg.value;
+                const expectedName = `Styled${tagName
+                    .charAt(0)
+                    .toUpperCase()}${tagName.slice(1)}`;
+
+                // Check if the variable name matches the expected pattern
+                if (variableName !== expectedName) {
+                    context.report({
+                        node: node.id,
+                        messageId: "errorString",
+                        data: {
+                            variableName,
+                            tagName,
+                            expectedName,
+                        },
+                    });
                 }
             },
         };
