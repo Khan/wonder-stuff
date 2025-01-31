07fa1c9e10dd53d56890b8e6ecdea0fcdf20c07d
Bea Esguerra <beaesguerra@khanacademy.org>
2025-01-31 15:51:55 -0700

    set up a11y-config.js based on index.js

---
+++ packages/eslint-config-khan/a11y-config.js
@@ -0,0 +1,27 @@
+/* eslint-disable import/no-commonjs */
+const OFF = "off";
+const WARN = "warn";
+const ERROR = "error";
+
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    plugins: ["@khanacademy"],
+    env: {
+        // TODO(csilvers): once we properly use node.js for node
+        // files, get rid of this next line.
+        node: true,
+        browser: true,
+        es6: true,
+        jest: true,
+    },
+    settings: {
+        react: {
+            version: "16.4",
+        },
+        "import/parsers": {
+            "@typescript-eslint/parser": [".ts", ".tsx"],
+        },
+        "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
+    },
+    rules: {},
+};
