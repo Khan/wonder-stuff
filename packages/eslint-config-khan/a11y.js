13ae38d03498839efbd834fe8d4a326cabc0ccab
Bea Esguerra <beaesguerra@khanacademy.org>
2025-02-04 09:41:33 -0700

    [eslint-a11y-config-khan] remove unneeded env settings

--- packages/eslint-config-khan/a11y.js
+++ packages/eslint-config-khan/a11y.js
@@ -5,12 +5,8 @@ module.exports = {
     parser: "@typescript-eslint/parser",
     plugins: ["@khanacademy"],
     env: {
-        // TODO(csilvers): once we properly use node.js for node
-        // files, get rid of this next line.
-        node: true,
         browser: true,
         es6: true,
-        jest: true,
     },
     settings: {
         react: {
