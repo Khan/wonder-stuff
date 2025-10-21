5abf3460a7fa42d0885369deaf8ccbdf956b8375
Jeff Yates <jeff@khanacademy.org>
2025-10-21 16:49:56 -0500

    [fei6388.2.renderserver] Delete render-server

--- packages/wonder-stuff-render-server/README.md
+++
@@ -1,3 +0,0 @@
-# wonder-stuff-render-server
-
-This Wonder Stuff package implements a generic render server for rendering a given URL using a configured render environment.
--- packages/wonder-stuff-render-server/src/start-trace-agent.ts
+++
@@ -1,7 +0,0 @@
-/**
- * This is the code for starting trace agent. Separating this from the
- * implementation allows us to to test the implementation itself separately.
- */
-import {startTraceAgent} from "./start-trace-agent-impl";
-
-startTraceAgent();
