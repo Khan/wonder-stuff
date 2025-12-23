a79d5bda353671a43de218b050597596004ab248
Jeremy Wiebe <jeremy@khanacademy.org>
2025-12-23 12:25:50 -0800

    Update packages/wonder-stuff-tool-new-pkg/src/generate-index-js.ts
    Co-authored-by: Jeff Yates <jeff@khanacademy.org>

--- packages/wonder-stuff-tool-new-pkg/src/generate-index-js.ts
+++ packages/wonder-stuff-tool-new-pkg/src/generate-index-js.ts
@@ -1,7 +1,8 @@
 /**
- * Generates the placholder package's `index.js` file contents. The contents are
- * meant to be very obvious that the package is simply a placeholder and
- * contains no usable code.
+ * Generate the placholder package's `index.js` file contents.
+ 
+ * The contents are meant to be very obvious that the package is simply
+ * a placeholder and contains no usable code.
  */
 export function generateIndexJs(): string {
     return 'throw new Error("Not implemented");\n';
